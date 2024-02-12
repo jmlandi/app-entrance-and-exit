d02d310b91820b088dddacb4495c2dcc33990ba5
Landi <joaomarcospsnbr@gmail.com>
2024-02-12 12:18:18 -0300

    V1-007 - Feedback menssage and duplicate fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 // default values and initialization
-const soundEffect = new Audio("footage/pop.mp3")
+document.querySelector("#scanner_output").style = "display: none;"
+const soundEffect = new Audio("/footage/pop.mp3")
 let bipType = window.document.querySelector("#bip_type")
 bipType.innerHTML = "ENTRADA"
 
@@ -15,6 +16,12 @@ document.querySelector("#bip_btn").addEventListener("click", function changeBipT
     }
 })
 
+document.querySelector("#scanner_output").addEventListener("click", function clearOutput() {
+    if (document.querySelector("#scanner_output").style.display != "none") {
+        document.querySelector("#scanner_output").style = "display: none;"
+    }
+})
+
 
 // QR code config
 const scanner = new Html5QrcodeScanner("reader", {
@@ -25,8 +32,10 @@ const scanner = new Html5QrcodeScanner("reader", {
 
 scanner.render(success, error)
 
+let lastData = ''
 function success(result) {
 
+    // Data from URL
     result = result.split("&")
     const id = result[0]
     const name = result[1]
@@ -34,20 +43,24 @@ function success(result) {
     const cargo = result[3]
     const date = new Date
     const datetime = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
-    // const party = result[4]
 
-    const postData = `Data=${datetime}&Tipo=${bipType.innerHTML}&ID QR Code=${id}&Nome=${name}&Loja=${store}&Cargo=${cargo}`
-    console.log("Dados Enviados")
-    postGoogleSheet(postData)
-
-    let response = document.querySelector("#scanner_output")
-    response.innerHTML = `<b>Novo registro</b><br>${bipType.innerHTML} | ${name}`
-
-    soundEffect.play()
+    // "POST" Google Sheet
+    if (document.querySelector("#scanner_output").style.display === "none") {
+        const postData = `Data=${datetime}&Tipo=${bipType.innerHTML}&ID QR Code=${id}&Nome=${name}&Loja=${store}&Cargo=${cargo}`
+        console.log("Dados Enviados")
+        postGoogleSheet(postData)
+        
+        // Feedback
+        let response = document.querySelector(".output_message")
+        document.querySelector("#scanner_output").style = "display: flex;"
+        response.innerHTML = `<p><b style="Font-size: 1.5rem; border-bottom: 1px solid black; padding: 10px 0px">Novo Registro 🚀</b><hr><br>${name} <b>(${bipType.innerHTML})</b></p>`
+        soundEffect.play()
+    }
+    
 }
 
 function error(err) {
-    // console.log(err)
+    console.log(err)
 }
 
 
